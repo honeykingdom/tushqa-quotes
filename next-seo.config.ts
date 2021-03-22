1f68d2170ca8bbfac9adf642065c097375f7d981
DmitryScaletta <dmitryscaletta@mail.ru>
2021-03-22 14:50:21 +0300

    🔧 Add next.config.js

---
+++ next-seo.config.ts
@@ -0,0 +1,12 @@
+import type { DefaultSeoProps } from "next-seo";
+
+const config: DefaultSeoProps = {
+  title: "Цитаты Тушки",
+  description: "Лучшие цитаты Тушки в чатах Ласки и Сегалла за всё время",
+  openGraph: {
+    type: "website",
+    locale: "ru_RU",
+  },
+};
+
+export default config;
