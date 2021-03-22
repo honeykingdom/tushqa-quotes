d19135ec225d9ce5683093e80f124aa2f5cb20ae
DmitryScaletta <dmitryscaletta@mail.ru>
2021-03-22 15:09:55 +0300

    ✨ Add main page

---
+++ src/pages/index.tsx
@@ -0,0 +1,82 @@
+import { useCallback, useEffect, useState } from "react";
+import * as R from "ramda";
+import { useSnackbar } from "notistack";
+import { Container, Box } from "@material-ui/core";
+import { useSession } from "next-auth/client";
+import Header from "components/Header";
+import QuoteCard from "components/QuoteCard";
+import Filters from "components/Filters";
+import SignInModal from "components/SignInModal";
+import filterQuotes from "utils/filterQuotes";
+import { usePostRatingQuery } from "generated/graphql";
+import type { FiltersType, RawQuoteType, QuoteType } from "types";
+
+import quotes from "../../data/quotes.json";
+
+export const defaultFilters: FiltersType = {
+  sortBy: "date",
+  sortOrder: "desc",
+  minCount: 0,
+};
+
+type Props = {
+  switchTheme: () => void;
+};
+
+const Home = ({ switchTheme }: Props) => {
+  const [session, authLoading] = useSession();
+  const [filters, setFilters] = useState<FiltersType>(defaultFilters);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpen = useCallback(() => setIsModalOpen(true), []);
+  const handleClose = useCallback(() => setIsModalOpen(false), []);
+
+  const { data, loading, refetch } = usePostRatingQuery();
+
+  useEffect(() => void refetch(), [session?.user]);
+
+  const modifiedQuotes: QuoteType[] = (quotes as RawQuoteType[]).map(
+    (quote) => {
+      const postRatingEntry = data?.postRating.find(
+        R.propEq("postId", quote.id)
+      );
+
+      const fullRating = postRatingEntry?.fullRating || 0;
+      const userRating = postRatingEntry?.userRating ?? null;
+
+      return {
+        ...quote,
+        fullRating,
+        userRating,
+      };
+    }
+  );
+
+  const renderedQuotes = filterQuotes(modifiedQuotes, filters);
+
+  const isDisabled = authLoading || loading;
+
+  return (
+    <>
+      <Header switchTheme={switchTheme} />
+      <Container maxWidth="md">
+        <Box sx={{ my: 4 }}>
+          <Filters changeFilters={setFilters} />
+          {renderedQuotes.map((quote) => (
+            <Box sx={{ mb: 2 }} key={quote.id}>
+              <QuoteCard
+                {...quote}
+                isDisabled={isDisabled}
+                isAuth={!!session}
+                onOpenSignInModal={handleOpen}
+              />
+            </Box>
+          ))}
+        </Box>
+      </Container>
+      <SignInModal open={isModalOpen} onClose={handleClose} />
+    </>
+  );
+};
+
+export default Home;
