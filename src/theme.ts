5fe274d0381cf97705600caca24c6b52c915ee61
DmitryScaletta <dmitryscaletta@mail.ru>
2021-03-25 21:44:47 +0300

    🩹 Update secondary color

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,9 @@ import { ThemeOptions } from "@material-ui/core/styles";
 const theme: ThemeOptions = {
   palette: {
     mode: "dark",
+    secondary: {
+      main: "#ff69b4",
+    },
   },
   typography: {
     htmlFontSize: 16,
