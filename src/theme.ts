bb9153f9890079ab6b33b313aab75a7576c8f661
DmitryScaletta <dmitryscaletta@mail.ru>
2021-07-02 11:22:17 +0300

    🩹 Add dark and light themes

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,15 @@
-import { ThemeOptions } from "@material-ui/core/styles";
+import * as R from "ramda";
+import { createTheme, ThemeOptions } from "@material-ui/core/styles";
 
-const theme: ThemeOptions = {
+const common: ThemeOptions = {
   palette: {
     mode: "dark",
-    secondary: {
+    thirdly: {
       main: "#ff69b4",
+    },
+    fourthly: {
+      main: "#6441a5",
+      contrastText: "#fff",
     },
   },
   typography: {
@@ -12,4 +17,28 @@ const theme: ThemeOptions = {
   },
 };
 
-export default theme;
+const dark: ThemeOptions = {
+  // @ts-expect-error
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#303030",
+      paper: "#424242",
+    },
+  },
+};
+const light: ThemeOptions = {
+  // @ts-expect-error
+  palette: {
+    mode: "light",
+  },
+};
+
+const themes = {
+  // @ts-expect-error
+  dark: createTheme(R.mergeDeepRight(common, dark)),
+  // @ts-expect-error
+  light: createTheme(R.mergeDeepRight(common, light)),
+};
+
+export default themes;
