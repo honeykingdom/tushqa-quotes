ce5d1f7e124ae3d73ea9b04c2f2b03eb6884530c
DmitryScaletta <dmitryscaletta@mail.ru>
2021-07-02 11:21:31 +0300

    ♻️ Use new material ui next js example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
-import * as R from "ramda";
+import { useState, useMemo, useCallback } from "react";
 import Head from "next/head";
 import { AppProps, NextWebVitalsMetric } from "next/app";
 import { Provider as AuthProvider } from "next-auth/client";
@@ -7,46 +6,24 @@ import { DefaultSeo } from "next-seo";
 // import { SnackbarProvider } from "notistack";
 import { ApolloProvider } from "@apollo/client";
 import { CssBaseline, PaletteMode } from "@material-ui/core";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-import defaultTheme from "theme";
+import themes from "theme";
 import { useApollo } from "apolloClient";
 import SEO from "../../next-seo.config";
 
-export const cache = createCache({ key: "css", prepend: true });
+const cache = createCache({ key: "css", prepend: true });
+cache.compat = true;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [paletteMode, setPaletteMode] = useState<PaletteMode>("dark");
   const apolloClient = useApollo(pageProps);
 
-  const theme = useMemo(
-    () =>
-      createMuiTheme(
-        R.mergeDeepRight(defaultTheme, { palette: { mode: paletteMode } })
-      ),
-    [paletteMode]
-  );
+  const theme = useMemo(() => themes[paletteMode], [paletteMode]);
 
   const toggleTheme = useCallback(() => {
     setPaletteMode(paletteMode === "dark" ? "light" : "dark");
-  }, [paletteMode]);
-
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement!.removeChild(jssStyles);
-    }
-  }, []);
-
-  // TODO: remove this
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const body = document.querySelector("body");
-
-      body!.style.backgroundColor = theme.palette.background.default;
-    }
   }, [paletteMode]);
 
   return (
