50021577bcc187859c66afe39ecc1bebad0b4d96
DmitryScaletta <dmitryscaletta@mail.ru>
2021-08-20 20:09:24 +0300

    ♻️ Use dynamic import instead of fs.readFile

--- src/server/utils/getQuotes.ts
+++ src/server/utils/getQuotes.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import { promises as fs } from "fs";
 import { QuoteType } from "../../types";
 
 export let quotes: QuoteType[] | null = null;
@@ -7,9 +6,8 @@ export let quotes: QuoteType[] | null = null;
 export const getQuotes = async () => {
   if (!quotes) {
     const quotesPath = path.resolve(process.cwd(), "data/quotes.json");
-    const quotesRaw = await fs.readFile(quotesPath, "utf8");
 
-    quotes = JSON.parse(quotesRaw) as QuoteType[];
+    quotes = (await import(quotesPath)) as QuoteType[];
   }
 
   return quotes;
